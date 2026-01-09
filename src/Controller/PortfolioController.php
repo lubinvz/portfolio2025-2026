d7f8048b53794e642c8af05cddc7cef4ac1c005c
lubin <lubin.vezant@etu.univ-st-etiennne.fr>
2026-01-09 15:56:29 +0100

    Ajout de mes modifications

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -8,10 +8,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PortfolioController extends AbstractController
 {
-        #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function home(): Response
     {
         return $this->render('portfolio/index.html.twig');
     }
 
+    #[Route('/about', name: 'app_propos')]
+    public function about(): Response
+    {
+        return $this->render('portfolio/propos.html.twig');
+    }
+
+    #[Route('/projects', name: 'app_projects')]
+    public function projects(): Response
+    {
+        return $this->render('portfolio/projet.html.twig');
+    }
+
+    #[Route('/cv', name: 'app_cv')]
+    public function contact(): Response
+    {
+        return $this->render('portfolio/cv.html.twig');
+    }
 }
